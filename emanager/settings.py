7c09092edf414b50aff3b3d1cd8749ee16cb8ddc
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2019-11-30 18:39:41 +0530

    register login and logout functionality

--- emanager/settings.py
+++ emanager/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'ehandler',
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'ehandler-home'
+LOGIN_URL = 'login'
--- emanager/urls.py
+++ emanager/urls.py
@@ -1,8 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ehandler.urls'))
+    path('', include('ehandler.urls')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.Profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
+
 ]
