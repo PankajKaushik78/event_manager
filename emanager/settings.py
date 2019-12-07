3de996439b13e55ca9dfdb87190fdb53e71e66d6
Pankaj Kaushik <kaushikpankaj78@gmail.com>
2019-12-07 11:01:21 +0530

    updated profile

--- emanager/settings.py
+++ emanager/settings.py
@@ -125,5 +125,8 @@ STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'ehandler:ehandler-home'
 LOGIN_URL = 'login'
